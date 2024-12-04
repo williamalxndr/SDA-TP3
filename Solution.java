ef162acef31f7e2fe88722f8cfd1b6ee17bbdc24
William Alexander <williamalexander@Williams-MacBook-Air.local>
2024-12-04 15:36:26 +0700

    Save computed R & implement edge with edge object (score: 15)

--- Solution.java
+++ Solution.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -11,17 +12,19 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class Solution {
     private static InputReader in;
-
+    private static PrintWriter out;
 
     public static void main(String[] args) {
 
         InputStream inputStream = System.in;
         in = new InputReader(inputStream);
-        // OutputStream outputStream = System.out;
-        // out = new PrintWriter(outputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
         int sofitaPosition = 1;
 
         int V = in.nextInteger();
@@ -49,11 +52,8 @@ public class Solution {
             switch (Qi) {
                 case "R":
                     int energi = in.nextInteger();
-                    Set<Integer> maxKotaVisited = graph.maximumKotaVisited(sofitaPosition, energi);
-                    if (maxKotaVisited.isEmpty()) System.out.println(-1);
-                    else System.out.println(maxKotaVisited.size());
-                    // System.out.println(maxKotaVisited.toString());
-                    
+
+                    out.println(R(graph, sofitaPosition, energi));
                     break;
 
                 case "F":
@@ -79,20 +79,40 @@ public class Solution {
                     break;
             }
         }
+        out.close();
     }
 
-    static int R(int energi) {
+    static int R(Graph graph, int sofitaPosition, int energi) {
         // Cetak jumlah kota maksimum yang dapat dikunjungi dengan energi sebanyak [ENERGI]
         // Setiap sampai di satu kota, sofita akan mengisi ulang energi
-        // TODO: Implement this method
-        return 0;
+
+        if (energi >= 100) return graph.size - 1;
+
+        Integer energyMinToTraverse = graph.energyNeededToTraverseAll[sofitaPosition];
+        if (energyMinToTraverse != null && energyMinToTraverse != 0 && energi >= energyMinToTraverse) {
+            // System.out.println("Get min energy ");
+            // System.out.println("Energy min: " + energyMinToTraverse);
+            return graph.size - 1;
+        }
+
+        Integer energyMaxToNotTraverse = graph.energyMaxToNotTraverse[sofitaPosition];
+        if (energyMaxToNotTraverse != null && energyMaxToNotTraverse != 0 && energi <= energyMaxToNotTraverse) return -1;
+
+        Integer getPrevOutput = graph.mapMaxKotaVisited.get(sofitaPosition).get(energi);
+        if (getPrevOutput != null) {
+            // System.out.println("Getprev");
+            return getPrevOutput;
+        }
+
+        int res = graph.computeMaxKotaVisited(sofitaPosition, energi);
+        // System.out.println("Res");
+        return res > 0 ? res : -1;
     }
 
-    static int F(int tujuan) {
+    static int F(Graph graph, int sofitaPosition, int tujuan) {
         // Cetak jarak terpendek ke kota dengan id [TUJUAN]
 
         // TODO: Implement this method
-        
         return 0;
     }
 
@@ -110,26 +130,37 @@ public class Solution {
 
     static class Graph {
         int size;
-        Map<Integer, List<Integer[]>> adjacencyList;  // fromId -> (toId, panjangJalan)
+        List<List<Edge>> adjacencyList;  // arr[from] = arr[to] = panjangJalan
+        int[] shortestCostSpanningTree;
+        int[] energyNeededToTraverseAll;
+        int[] energyMaxToNotTraverse;
+        Map<Integer, Map<Integer, Integer>> mapMaxKotaVisited;  // Menyimpan output query R yang sudah dilakukan agar tidak double compute maxKotaVisited, id -> (energi -> maxKota)
 
         Graph(int V) {
             size = V;
-            adjacencyList = new HashMap<>();
-            for (int i=1; i<=V; i++) {
-                adjacencyList.put(i, new ArrayList<Integer[]>());
+            adjacencyList = new ArrayList<>();
+            mapMaxKotaVisited = new HashMap<>();
+            for (int i = 0; i < V+1; i++) {
+                adjacencyList.add(new ArrayList<>());
+                mapMaxKotaVisited.put(i, new HashMap<>());
             }
+            energyNeededToTraverseAll = new int[V+1];
+            energyMaxToNotTraverse = new int[V+1];
         }
 
         void addEdge(int from, int to, int panjang) {
-            List<Integer[]> fromList = adjacencyList.get(from);
-            fromList.add(new Integer[]{to, panjang});
-            List<Integer[]> toList = adjacencyList.get(to);
-            toList.add(new Integer[]{from, panjang});
+            List<Edge> fromList = adjacencyList.get(from);
+            fromList.add(new Edge(to, panjang));
+            List<Edge> toList = adjacencyList.get(to);
+            toList.add(new Edge(from, panjang));
         }
 
-        Set<Integer> maximumKotaVisited(int from, int energi) {
-            Set<Integer> visited = new HashSet<>();
+        int computeMaxKotaVisited(int from, int energi) {
             Queue<Integer> queue = new LinkedList<>();
+            boolean[] visitedKota = new boolean[size+1];
+
+            int visited = 0;
+            visitedKota[from] = true;
 
             // Doing breadth first search
             queue.add(from);
@@ -137,26 +168,41 @@ public class Solution {
             while(!queue.isEmpty()) {
                 int explore = queue.poll();
                 
-                List<Integer[]> jalanan = adjacencyList.get(explore);
-                for (Integer[] jalan : jalanan) {
-                    int toId = jalan[0];
-                    int panjangJalan = jalan[1];
-                    if (!visited.contains(toId) && energi >= panjangJalan && toId != from) {
-                        visited.add(toId);
-                        queue.add(toId);
+                List<Edge> jalanan = adjacencyList.get(explore);
+                for (Edge jalan : jalanan) {
+                    int toId = jalan.toId;
+                    int panjangJalan = jalan.panjangJalan;
+                    if (!visitedKota[toId] && energi >= panjangJalan && toId != from) {
+                        visitedKota[toId] = true;
+                        queue.offer(toId);
+                        visited++;
                     }
                 }
-
             }
+
+            if (visited == (size-1)) energyNeededToTraverseAll[from] = energi;
+            else if (visited == 0) energyMaxToNotTraverse[from] = energi;
+
+            mapMaxKotaVisited.get(from).put(energi, visited);
+            
             return visited;
         }
 
+
         Long[] dijkstra(int from) {
-            return new Long[1];
+            
         }
 
+    }
 
+    static class Edge {
+        int toId;
+        int panjangJalan;
 
+        Edge(int to, int panjang) { 
+            toId = to;
+            panjangJalan = panjang;
+        }
     }
 
 
